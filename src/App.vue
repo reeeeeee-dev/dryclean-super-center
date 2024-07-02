b46a51fedbe0047e057a40974186a0de1f2f8af9
Reetik Patel <reetikp@gmail.com>
2024-07-02 15:23:48 -0500

    💄 navbar

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,28 @@ import { RouterView } from 'vue-router'
 <template>
   <header>
     <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/locations">Locations</RouterLink>
-        <RouterLink to="/services">Services</RouterLink>
-        <RouterLink to="/about">About Us</RouterLink>
+      <nav class="flex justify-end items-center px-8 bg-[var(--dark)] text-white h-16">
+        <RouterLink class="h-full p-4 place-content-center hover:bg-[var(--tertiary)]" to="/">
+          Home
+        </RouterLink>
+        <RouterLink
+          class="h-full p-4 place-content-center hover:bg-[var(--tertiary)] transition-colors"
+          to="/locations"
+        >
+          Locations
+        </RouterLink>
+        <RouterLink
+          class="h-full p-4 place-content-center hover:bg-[var(--tertiary)] transition-colors"
+          to="/services"
+        >
+          Services
+        </RouterLink>
+        <RouterLink
+          class="h-full p-4 place-content-center hover:bg-[var(--tertiary)] transition-colors"
+          to="/about"
+        >
+          About Us
+        </RouterLink>
       </nav>
     </div>
   </header>
