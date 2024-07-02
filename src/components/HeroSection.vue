78072f387f83d4f08312962bfa72cd8ff322efc8
Reetik Patel <reetikp@gmail.com>
2024-07-02 13:51:09 -0500

    🚧 added bubble to hero

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="flex flex-row gap-40 w-screen p-16 items-center">
+    <div class="bubble" />
     <div class="w-[50%]">
       <h1 class="text-9xl">Dryclean Super Center</h1>
       <h4 class="mt-6 text-xl">Dry all your worries away</h4>
@@ -9,3 +10,17 @@
     </div>
   </div>
 </template>
+
+<style scoped>
+.bubble {
+  background: url('/bubble.jpg');
+  background-size: contain;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  z-index: -1;
+  filter: blur(100px);
+}
+</style>
