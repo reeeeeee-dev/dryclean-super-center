a1068206108e42255f17e1e99421c86187ca4304
Reetik Patel <reetikp@gmail.com>
2024-07-02 12:55:16 -0500

    ➕ prime vue (lara)

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import PrimeView from 'primevue/config'
+import Lara from '@/presets/lara'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+app.use(PrimeView, {
+  unstyled: true,
+  pt: Lara
+})
 
 app.use(router)
 
