74bc13c56e31769eac7d9c328576e3a660c0ff6c
Reetik Patel <reetikp@gmail.com>
2024-07-02 13:35:52 -0500

    🚧 basic services section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <script setup>
 import HeroSection from '@/components/HeroSection.vue'
 import LinksSection from '@/components/LinksSection/LinksSection.vue'
+import AdditionalServicesSection from '@/components/AdditionalServicesSection.vue'
 </script>
 
 <template>
   <main class="">
     <HeroSection />
     <LinksSection />
+    <AdditionalServicesSection />
   </main>
 </template>
 
