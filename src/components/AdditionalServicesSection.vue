28f33c368f0d9d12a4629247c5c12d796d6a2a44
Reetik Patel <reetikp@gmail.com>
2024-07-02 15:12:47 -0500

    💄 added 3D effects

--- src/components/AdditionalServicesSection.vue
+++ src/components/AdditionalServicesSection.vue
@@ -4,38 +4,70 @@
     <h4 class="mt-8 text-4xl">Let our experts take care of all your garmet cleaning needs!</h4>
 
     <div class="mt-32 grid grid-cols-4 gap-y-24">
-      <div class="text-4xl flex flex-col gap-4 items-center">
-        <img class="size-32 rounded-full border" src="https://picsum.photos/200?seed=1" />
-        <span>Dry Cleaning</span>
-      </div>
-      <div class="text-4xl flex flex-col gap-4 items-center">
-        <img class="size-32 rounded-full" src="https://picsum.photos/200?seed=2" />
-        <span>Dry Cleaning</span>
-      </div>
-      <div class="text-4xl flex flex-col gap-4 items-center">
-        <img class="size-32 rounded-full" src="https://picsum.photos/200?seed=3" />
-        <span>Dry Cleaning</span>
-      </div>
-      <div class="text-4xl flex flex-col gap-4 items-center">
-        <img class="size-32 rounded-full" src="https://picsum.photos/200?seed=4" />
-        <span>Dry Cleaning</span>
-      </div>
-      <div class="text-4xl flex flex-col gap-4 items-center">
-        <img class="size-32 rounded-full" src="https://picsum.photos/200?seed=5" />
-        <span>Dry Cleaning</span>
-      </div>
-      <div class="text-4xl flex flex-col gap-4 items-center">
-        <img class="size-32 rounded-full" src="https://picsum.photos/200?seed=6" />
-        <span>Dry Cleaning</span>
-      </div>
-      <div class="text-4xl flex flex-col gap-4 items-center">
-        <img class="size-32 rounded-full" src="https://picsum.photos/200?seed=7" />
-        <span>Dry Cleaning</span>
-      </div>
-      <div class="text-4xl flex flex-col gap-4 items-center">
-        <img class="size-32 rounded-full" src="https://picsum.photos/200?seed=8" />
-        <span>Dry Cleaning</span>
-      </div>
+      <kinesis-container>
+        <kinesis-element :strength="10" type="depth">
+          <div class="text-4xl flex flex-col gap-4 items-center">
+            <img class="size-32 rounded-full border" src="https://picsum.photos/200?seed=1" />
+            <span>Dry Cleaning</span>
+          </div>
+        </kinesis-element>
+      </kinesis-container>
+      <kinesis-container>
+        <kinesis-element :strength="10" type="depth">
+          <div class="text-4xl flex flex-col gap-4 items-center">
+            <img class="size-32 rounded-full" src="https://picsum.photos/200?seed=2" />
+            <span>Dry Cleaning</span>
+          </div>
+        </kinesis-element>
+      </kinesis-container>
+      <kinesis-container>
+        <kinesis-element :strength="10" type="depth">
+          <div class="text-4xl flex flex-col gap-4 items-center">
+            <img class="size-32 rounded-full" src="https://picsum.photos/200?seed=3" />
+            <span>Dry Cleaning</span>
+          </div>
+        </kinesis-element>
+      </kinesis-container>
+      <kinesis-container>
+        <kinesis-element :strength="10" type="depth">
+          <div class="text-4xl flex flex-col gap-4 items-center">
+            <img class="size-32 rounded-full" src="https://picsum.photos/200?seed=4" />
+            <span>Dry Cleaning</span>
+          </div>
+        </kinesis-element>
+      </kinesis-container>
+      <kinesis-container>
+        <kinesis-element :strength="10" type="depth">
+          <div class="text-4xl flex flex-col gap-4 items-center">
+            <img class="size-32 rounded-full" src="https://picsum.photos/200?seed=5" />
+            <span>Dry Cleaning</span>
+          </div>
+        </kinesis-element>
+      </kinesis-container>
+      <kinesis-container>
+        <kinesis-element :strength="10" type="depth">
+          <div class="text-4xl flex flex-col gap-4 items-center">
+            <img class="size-32 rounded-full" src="https://picsum.photos/200?seed=6" />
+            <span>Dry Cleaning</span>
+          </div>
+        </kinesis-element>
+      </kinesis-container>
+      <kinesis-container>
+        <kinesis-element :strength="10" type="depth">
+          <div class="text-4xl flex flex-col gap-4 items-center">
+            <img class="size-32 rounded-full" src="https://picsum.photos/200?seed=7" />
+            <span>Dry Cleaning</span>
+          </div>
+        </kinesis-element>
+      </kinesis-container>
+      <kinesis-container>
+        <kinesis-element :strength="10" type="depth">
+          <div class="text-4xl flex flex-col gap-4 items-center">
+            <img class="size-32 rounded-full" src="https://picsum.photos/200?seed=8" />
+            <span>Dry Cleaning</span>
+          </div>
+        </kinesis-element>
+      </kinesis-container>
     </div>
   </div>
 </template>
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,22 +1,29 @@
+<script setup></script>
+
 <template>
   <div class="flex flex-row gap-40 w-screen p-16 items-center">
-    <div class="bubble" />
+    <img class="bubble" src="/bubble.jpg" />
     <div class="w-[50%]">
       <h1 class="text-9xl">Dryclean Super Center</h1>
       <h4 class="mt-6 text-xl">Dry all your worries away</h4>
     </div>
     <div class="w-[50%] flex justify-center">
-      <img src="/blankets.jpg" class="object-cover rounded-full h-[70vh] w-[70vh]" />
+      <kinesis-container>
+        <kinesis-element :strength="10" type="depth">
+          <img
+            src="/blankets.jpg"
+            class="object-cover rounded-full max-h-[70vh] w-[70vh] aspect-square"
+          />
+        </kinesis-element>
+      </kinesis-container>
     </div>
   </div>
 </template>
 
 <style scoped>
 .bubble {
-  background: url('/bubble.jpg');
-  background-size: contain;
   position: absolute;
-  top: 0;
+  top: 10vh;
   left: 0;
   height: 100vh;
   width: 100vw;
