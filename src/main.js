0ac30861765cb148c330e5c481015ca8d4adf476
Reetik Patel <reetikp@gmail.com>
2024-07-02 15:12:16 -0500

    ➕vue-kinesis

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueKinesis from 'vue-kinesis'
+import { KinesisContainer, KinesisElement } from 'vue-kinesis'
 
 const app = createApp(App)
 
 app.use(router)
+
+app.use(VueKinesis)
+app.component('kinesis-container', KinesisContainer)
+app.component('kinesis-element', KinesisElement)
 
 app.mount('#app')
