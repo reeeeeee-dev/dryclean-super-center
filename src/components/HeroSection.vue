000acb457c111b14bd9075e6836b9b8b620596f4
Reetik Patel <reetikp@gmail.com>
2024-07-05 15:44:12 -0500

    🎨 removed style from hero

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="flex flex-row gap-40 w-screen p-16 items-center">
-    <img class="bubble" src="/bubble.jpg" />
+    <img class="absolute top-[10vh] left-0 w-screen -z-10 blur-[100px]" src="/bubble.jpg" />
     <div class="w-[50%]">
       <h1 class="text-9xl">Dryclean Super Center</h1>
       <h4 class="mt-6 text-xl">Dry all your worries away</h4>
@@ -19,15 +19,3 @@
     </div>
   </div>
 </template>
-
-<style scoped>
-.bubble {
-  position: absolute;
-  top: 10vh;
-  left: 0;
-  height: 100vh;
-  width: 100vw;
-  z-index: -1;
-  filter: blur(100px);
-}
-</style>
