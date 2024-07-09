a28928ac6aa639e9d5e3cb32e3ea6e821eee943e
Reetik Patel <reetikp@gmail.com>
2024-07-08 23:32:23 -0500

    🏗️ moved header to component

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,12 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import HeaderComponent from '@/components/HeaderComponent.vue'
 import FooterSection from '@/components/FooterSection.vue'
 </script>
 
 <template>
-  <header>
-    <div>
-      <nav class="flex justify-end items-center px-8 bg-[var(--dark)] text-white h-16">
-        <RouterLink class="h-full p-4 place-content-center hover:bg-[var(--tertiary)]" to="/">
-          Home
-        </RouterLink>
-        <RouterLink
-          class="h-full p-4 place-content-center hover:bg-[var(--tertiary)] transition-colors"
-          to="/locations"
-        >
-          Locations
-        </RouterLink>
-        <RouterLink
-          class="h-full p-4 place-content-center hover:bg-[var(--tertiary)] transition-colors"
-          to="/services"
-        >
-          Services
-        </RouterLink>
-        <RouterLink
-          class="h-full p-4 place-content-center hover:bg-[var(--tertiary)] transition-colors"
-          to="/about"
-        >
-          About Us
-        </RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <HeaderComponent />
   <RouterView class="main" />
-
   <FooterSection />
 </template>
 
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <div>
+      <nav class="flex justify-end items-center px-8 bg-[var(--dark)] text-white h-16">
+        <RouterLink class="h-full p-4 place-content-center hover:bg-[var(--tertiary)]" to="/">
+          Home
+        </RouterLink>
+        <RouterLink
+          class="h-full p-4 place-content-center hover:bg-[var(--tertiary)] transition-colors"
+          to="/locations"
+        >
+          Locations
+        </RouterLink>
+        <RouterLink
+          class="h-full p-4 place-content-center hover:bg-[var(--tertiary)] transition-colors"
+          to="/services"
+        >
+          Services
+        </RouterLink>
+        <RouterLink
+          class="h-full p-4 place-content-center hover:bg-[var(--tertiary)] transition-colors"
+          to="/about"
+        >
+          About Us
+        </RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
