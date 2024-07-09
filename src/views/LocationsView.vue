9e8fbb2d2ff170517f9024160029a53d95277baa
Reetik Patel <reetikp@gmail.com>
2024-07-08 19:44:47 -0500

    🚧 locations content

--- src/views/LocationsView.vue
+++ src/views/LocationsView.vue
@@ -1,3 +1,59 @@
 <template>
-  <div>TODO: Insert locations here</div>
+  <div class="m-16">
+    <div class="mb-16">
+      <h1 class="text-7xl mb-4">Two Convenient Locations</h1>
+      <p class="max-w-prose">
+        TODO: Pariatur fugiat consectetur deserunt voluptate. Fugiat adipisicing elit voluptate non
+        et aliqua cillum et sunt sint aliquip reprehenderit. Occaecat et pariatur nisi aliqua nulla
+        ex elit commodo velit dolor nisi tempor Lorem ex. Anim officia exercitation aute consequat
+        commodo minim dolore laboris.
+      </p>
+    </div>
+
+    <div class="flex">
+      <div>TODO: Insert Photo</div>
+      <div class="flex flex-col gap-3">
+        <h3 class="text-3xl">Collierville</h3>
+        <a href="tel:9018535540" class="text-xl hover:underline hover:cursor-pointer">
+          (901) 853-5540
+        </a>
+        <p class="font-bold">
+          520 Market Blvd <br />
+          Collierville, TN 38017
+        </p>
+        <p>
+          Monday-Friday: 7 AM to 7 PM
+          <br />
+          Saturday: 8 AM to 4 PM
+          <br />
+          Sunday: Closed
+        </p>
+      </div>
+      <div>TODO: Insert Map</div>
+    </div>
+
+    <hr class="border border-dashed border-[var(--secondary)] my-16" />
+
+    <div class="flex">
+      <div>TODO: Insert Photo</div>
+      <div class="flex flex-col gap-3">
+        <h3 class="text-3xl">Southaven</h3>
+        <a href="tel:6628938142" class="text-xl hover:underline hover:cursor-pointer">
+          (662) 893-8142
+        </a>
+        <p class="font-bold">
+          3964 Goodman Rd E #103 <br />
+          Southaven, MS 38672
+        </p>
+        <p>
+          Monday-Friday: 7 AM to 6 PM
+          <br />
+          Saturday: 8 AM to 4 PM
+          <br />
+          Sunday: Closed
+        </p>
+      </div>
+      <div>TODO: Insert Map</div>
+    </div>
+  </div>
 </template>
