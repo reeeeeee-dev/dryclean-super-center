8ee3bec22216ded786ff36bdf2629a4525c99d05
Reetik Patel <reetikp@gmail.com>
2024-07-09 12:04:35 -0500

    🚧✨ added map

--- src/views/LocationsView.vue
+++ src/views/LocationsView.vue
@@ -1,3 +1,15 @@
+<script setup>
+import { GoogleMap, Marker } from 'vue3-google-map'
+const MAPS_API_KEY = 'AIzaSyDnVPefEgyKOvXeHHPuRemarCQVurfRo9w'
+const colliervilleMarker = {
+  position: { lat: 35.053719522949784, lng: -89.69167306911073 }
+}
+const southavenMarker = {
+  position: { lat: 34.964451578835465, lng: -89.92062262709256 }
+}
+const roughMidpoint = { lat: 35.026005785199175, lng: -89.82968555807594 }
+</script>
+
 <template>
   <div class="m-16">
     <div class="mb-16">
@@ -11,7 +23,6 @@
     </div>
 
     <div class="flex">
-      <div>TODO: Insert Photo</div>
       <div class="flex flex-col gap-3">
         <h3 class="text-3xl">Collierville</h3>
         <a href="tel:9018535540" class="text-xl hover:underline hover:cursor-pointer">
@@ -29,13 +40,12 @@
           Sunday: Closed
         </p>
       </div>
-      <div>TODO: Insert Map</div>
+      <div>TODO: Insert Photo</div>
     </div>
 
     <hr class="border border-dashed border-[var(--secondary)] my-16" />
 
     <div class="flex">
-      <div>TODO: Insert Photo</div>
       <div class="flex flex-col gap-3">
         <h3 class="text-3xl">Southaven</h3>
         <a href="tel:6628938142" class="text-xl hover:underline hover:cursor-pointer">
@@ -53,7 +63,14 @@
           Sunday: Closed
         </p>
       </div>
-      <div>TODO: Insert Map</div>
+      <div>TODO: Insert Photo</div>
     </div>
+
+    <br class="my-16" />
+
+    <GoogleMap class="h-96 w-full" :zoom="11" :center="roughMidpoint" :api-key="MAPS_API_KEY">
+      <Marker :options="colliervilleMarker" />
+      <Marker :options="southavenMarker" />
+    </GoogleMap>
   </div>
 </template>
