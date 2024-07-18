038faad01e9708e9af584db163bee897f7ae85ce
Reetik Patel <reetikp@gmail.com>
2024-07-17 22:20:51 -0500

    💄🎨 destructured header and added shadow

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,32 +1,30 @@
 <template>
-  <header>
-    <div>
-      <nav class="flex justify-between items-center px-8 bg-[var(--dark)] text-white h-16">
-        <h1 class="float-start">Dry Clean Super Center</h1>
-        <div>
-          <RouterLink class="h-full p-4 place-content-center hover:bg-[var(--tertiary)]" to="/">
-            Home
-          </RouterLink>
-          <RouterLink
-            class="h-full p-4 place-content-center hover:bg-[var(--tertiary)] transition-colors"
-            to="/locations"
-          >
-            Locations
-          </RouterLink>
-          <RouterLink
-            class="h-full p-4 place-content-center hover:bg-[var(--tertiary)] transition-colors"
-            to="/services"
-          >
-            Services
-          </RouterLink>
-          <RouterLink
-            class="h-full p-4 place-content-center hover:bg-[var(--tertiary)] transition-colors"
-            to="/about"
-          >
-            About Us
-          </RouterLink>
-        </div>
-      </nav>
-    </div>
+  <header class="bg-[var(--dark)] shadow-xl sticky top-0 z-50">
+    <nav class="flex justify-between items-center fade-in px-8 text-[var(--light)] h-16">
+      <h1 class="float-start">Dry Clean Super Center</h1>
+      <div>
+        <RouterLink class="h-full p-4 place-content-center hover:bg-[var(--tertiary)]" to="/">
+          Home
+        </RouterLink>
+        <RouterLink
+          class="h-full p-4 place-content-center hover:bg-[var(--tertiary)] transition-colors"
+          to="/locations"
+        >
+          Locations
+        </RouterLink>
+        <RouterLink
+          class="h-full p-4 place-content-center hover:bg-[var(--tertiary)] transition-colors"
+          to="/services"
+        >
+          Services
+        </RouterLink>
+        <RouterLink
+          class="h-full p-4 place-content-center hover:bg-[var(--tertiary)] transition-colors"
+          to="/about"
+        >
+          About Us
+        </RouterLink>
+      </div>
+    </nav>
   </header>
 </template>
