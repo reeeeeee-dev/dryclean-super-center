0292c5dbf6129322f5df4d2431772089c660b35d
Reetik Patel <reetikp@gmail.com>
2024-07-17 22:42:14 -0500

    🚧 added privacy policy content

--- src/views/LegalView.vue
+++ src/views/LegalView.vue
@@ -1,3 +1,49 @@
 <template>
-  <div>TODO: Insert Privacy Policy here</div>
+  <div class="p-16 max-w-prose m-auto">
+    <h1 class="text-4xl mb-4">Privacy Policy</h1>
+
+    <h4 class="text-xl font-bold mt-4">Our Commitment To Privacy</h4>
+    <p>
+      Your privacy is important to us. To better protect your privacy, we provide this notice
+      explaining our online information practices and the choices you can make about the way your
+      information is collected and used. To make this notice easy to find, we make it available on
+      our homepage and at every point where personally identifiable information may be requested.
+    </p>
+
+    <h4 class="text-xl font-bold mt-4">The Information We Collect</h4>
+    <p>
+      This notice applies to all information collected or submitted on the Dry Clean Super Center
+      website. On some pages, you can order products, make requests, and register to receive
+      materials. The types of personal information collected at these pages are:
+    </p>
+    <ul class="ml-8 list-disc">
+      <li>Name</li>
+      <li>Address</li>
+      <li>Email Address</li>
+      <li>Phone Number</li>
+      <li>Credit/Debit Card Information</li>
+    </ul>
+
+    <h4 class="text-xl font-bold mt-4">The Way We Use Information</h4>
+    <p>
+      We use the information you provide about yourself when placing an order only to complete that
+      order. We do not share this information with outside parties. We use return email addresses to
+      answer the email we receive. Such addresses are not used for any other purpose and are not
+      shared with outside parties. Finally, we never use or share the personally identifiable
+      information provided to us online in ways unrelated to the ones described above.
+    </p>
+
+    <h4 class="text-xl font-bold mt-4">Our Commitment To Data Security</h4>
+    <p>
+      To prevent unauthorized access, maintain data accuracy, and ensure the correct use of your
+      information, we have put in place appropriate physical, electronic, and managerial procedures
+      to safeguard and secure the information we collect online.
+    </p>
+
+    <h4 class="text-xl font-bold mt-4">How To Contact Us</h4>
+    <p>
+      Should you have other questions or concerns about this policy, please use the
+      <RouterLink to="/contact">Contact page</RouterLink> to get in touch with us.
+    </p>
+  </div>
 </template>
