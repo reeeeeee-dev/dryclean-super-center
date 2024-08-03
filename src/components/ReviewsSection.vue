00ae25b1f16a76341b134c471c04acd7f8680e1b
Reetik Patel <reetikp@gmail.com>
2024-08-03 15:34:56 -0500

    🐛 fixed reviews overflow

--- src/components/ReviewsSection.vue
+++ src/components/ReviewsSection.vue
@@ -23,10 +23,10 @@ const reviews = [
     v-prlx.background="{ fromBottom: true }"
   >
     <h1 class="text-5xl font-bold mb-16">Hear from our customers</h1>
-    <div class="w-96 m-auto">
+    <div class="m-auto">
       <carousel wrap-around pause-autoplay-on-hover :items-to-show="1" autoplay="5000">
-        <Slide class="max-w-96" v-for="slide in reviews" :key="slide">
-          <div class="text-left">
+        <Slide class="" v-for="slide in reviews" :key="slide">
+          <div class="text-left mx-24">
             <p>{{ slide.text }}</p>
             <em>- {{ slide.author }}</em>
           </div>
