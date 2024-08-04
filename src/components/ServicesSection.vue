a85f1532bf421f01a9947b8d53f7ca34a745d010
Reetik Patel <reetikp@gmail.com>
2024-08-04 13:19:09 -0500

    🐛 added routing and cleaned up services

--- src/components/ServicesSection.vue
+++ src/components/ServicesSection.vue
@@ -48,15 +48,17 @@ const services = [
     <h1 class="text-7xl font-bold">Our Services</h1>
     <h4 class="mt-8 text-3xl font-light">Let our experts take care of all your garmet needs!</h4>
 
-    <div class="mt-32 grid grid-cols-4 gap-y-24">
-      <kinesis-container v-for="service in services" :key="service.text">
-        <div class="text-4xl flex flex-col gap-4 items-center">
-          <kinesis-element strength="-10" type="rotate">
-            <img class="size-32 rounded-full border" :src="service.img" />
-          </kinesis-element>
-          <span>{{ service.title }}</span>
-        </div>
-      </kinesis-container>
+    <div class="mt-32 grid grid-cols-4 gap-y-24 gap-x-8 max-lg:grid-cols-3 max-sm:grid-cols-1">
+      <RouterLink v-for="service in services" :key="service.text" :to="service.link">
+        <kinesis-container class="bg-[var(--light)] text-[var(--dark)] rounded-3xl p-8 h-96">
+          <div class="text-4xl flex flex-col gap-4 items-center">
+            <kinesis-element strength="-10" type="rotate">
+              <img class="size-32 rounded-full border" :src="service.img" />
+            </kinesis-element>
+            <span>{{ service.title }}</span>
+          </div>
+        </kinesis-container>
+      </RouterLink>
     </div>
   </div>
 </template>
