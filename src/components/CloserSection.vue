4a0ccfe6841eebf51f349cfdcd18a9f62efdce56
Reetik Patel <reetikp@gmail.com>
2024-08-07 21:22:11 -0500

    💄adjusted heading spacing in closer

--- src/components/CloserSection.vue
+++ src/components/CloserSection.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-8 max-w-prose m-auto my-8 rounded-3xl bg-dark">
-    <h3 class="text-2xl">Visit Us Today</h3>
+    <h3 class="text-2xl mb-4">Visit Us Today</h3>
     <p>
       Discover the Dry Clean Super Center difference. Visit one of our convenient locations, or take
       advantage of our pick-up and delivery service. Your clothes deserve the best care, and we are
