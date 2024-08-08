aea3b11234f86b51cf428eeff4fa978483e9bad8
Reetik Patel <reetikp@gmail.com>
2024-08-07 21:28:18 -0500

    🐛 fixed not scrolling to top on nav

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,14 @@ import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior: (to, _from, savedPosition) => {
+    if (savedPosition) return savedPosition
+    if (to.hash) {
+      return { el: to.hash, behavior: 'smooth' }
+    } else {
+      window.scrollTo({ top: 0, behavior: 'smooth' })
+    }
+  },
   routes: [
     // Main pages
     {
