b83b7501e0b859d24fdac48248e3850fce8cc820
Reetik Patel <reetikp@gmail.com>
2024-08-09 00:08:27 -0500

    ➕ vite-plugin-md

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
+import Markdown from 'vite-plugin-md'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    Vue({
+      include: [/\.vue$/, /\.md$/]
+    }),
+    Markdown()
   ],
   resolve: {
     alias: {
