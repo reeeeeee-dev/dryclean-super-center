6d67b66241e1b9bf8537f1ec37cef8461476553b
Reetik Patel <reetikp@gmail.com>
2024-08-09 00:16:49 -0500

    ♿ added back button and fixed hover

--- src/views/BlogPostView.vue
+++ src/views/BlogPostView.vue
@@ -6,7 +6,12 @@ const Markdown = defineAsyncComponent(() => import(`@/blog/${props.postName}.md`
 </script>
 
 <template>
-  <div>
+  <div class="m-auto w-max">
+    <RouterLink to="/blog" class="underline hover:text-[var(--accent)] transition-colors">
+      ← Back to blog
+    </RouterLink>
+    <br />
+    <br />
     <div class="m-auto rounded-3xl p-8 bg-[var(--light)] w-max h-min">
       <Markdown class="prose" />
     </div>
--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -14,7 +14,7 @@ const posts = Object.entries(import.meta.glob('@/blog/*.md', { eager: true })).m
       v-for="post in posts"
       :key="post.key"
       :to="`/blog/${post.path}`"
-      class="rounded-3xl bg-[var(--light)] p-8 h-64 w-96"
+      class="rounded-3xl bg-[var(--light)] p-8 h-64 w-96 hover:bg-[var(--accent)] hover:text-[var(--light)] transition-colors"
     >
       <h3 class="text-2xl font-bold mb-4">{{ post.title }}</h3>
       <p>{{ post.summary }}</p>
